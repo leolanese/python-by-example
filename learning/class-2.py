dd45b532d3bf11affdac74b3c4b976586cc0b5c6
Leo Lanese <laneseleo@gmail.com>
2018-10-15 11:43:48 +0100

    Create class-2.py

---
+++ learning/class-2.py
@@ -0,0 +1,24 @@
+class myClass(): # create a class
+  def foo1(self): # create a function in the class
+    print ("myClass foo1")
+    
+  def foo2(self, someString):
+    print ("myClass foo2: " + someString)
+    
+class anotherClass(myClass): # create a another class
+  def foo2(self): # create a function in the other class
+    print ("anotherClass foo2")
+    
+  def foo1(self):
+    myClass.foo1(self);
+    print ("anotherClass foo1")
+      
+def main():
+  c = myClass()
+  c.foo1()
+  c.foo2("This is a string")
+  c2 = anotherClass()
+  c2.foo1()
+  
+if __name__ == "__main__":
+  main()
