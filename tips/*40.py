a36d4cc2cb49624b51ba7830fb758da0381d0cde
Leo Lanese <laneseleo@gmail.com>
2018-10-16 09:38:50 +0100

    Create *40.py

---
+++ tips/*40.py
@@ -0,0 +1,11 @@
+print("Hello!, Welcome to Python")
+print("*" * 40)
+print("Please Enter your name ")
+name = input()
+print("Your name is, " + name)
+
+# Hello!,Welcome to Python
+# ****************************************
+# Please Enter your name 
+# Leo
+# Your name is, Leo
