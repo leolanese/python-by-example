fff21cce9eeb13cd06da5e2eea53106e34296ce8
Leo Lanese <laneseleo@gmail.com>
2018-10-22 15:07:17 +0100

    Create pprint.py

---
+++ tips/pprint.py
@@ -0,0 +1,18 @@
+# The standard string repr for dicts is hard to read:
+>>> my_mapping = {'a': 23, 'b': 42, 'c': 0xc0ffee}
+>>> my_mapping
+{'b': 42, 'c': 12648430. 'a': 23}  
+
+# The "json" module can do a much better job:
+import json
+print(json.dumps(my_mapping, indent=4, sort_keys=True))
+
+# {
+#   "a": 23,
+#   "b": 42,
+#   "c": 12648430
+# }
+
+# Note this only works with dicts containing
+# primitive types (check out the "pprint" module):
+json.dumps({all: 'yup'}) # TypeError: keys must be a string
