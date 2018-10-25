b06970c961f502c6585556ad20343154faaa07ea
Leo Lanese <laneseleo@gmail.com>
2018-10-25 12:04:07 +0100

    Update sort-dict- by-value.py

--- tips/sort-dict- by-value.py
+++ tips/sort-dict- by-value.py
@@ -1,11 +1,25 @@
 # How to sort a Python dict by value
 # (== get a representation sorted by value)
 
+# 1
 xs = {'a': 4, 'b': 3, 'c': 2, 'd': 1}
-
 sorted(xs.items(), key=lambda x: x[1]) # [('d', 1), ('c', 2), ('b', 3), ('a', 4)]
 
-# Or:
+# 2
+Weight_details = {
+    'Sam':45, 
+    'Irum':67,
+    'Dolly':80, 
+    'Bela':20
+    }
+ 
+sorted_weight = sorted(Weight_details.items(), key=lambda x:x[1])
+print(sorted_weight)
 
+# 3: using sort & Lambda Function
 import operator
 sorted(xs.items(), key=operator.itemgetter(1)) # [('d', 1), ('c', 2), ('b', 3), ('a', 4)]
+
+# 3: using sort & Lambda Function & reverse
+import operator
+sorted(xs.items(), key=operator.itemgetter(1), reverse=True) # [('d', 1), ('c', 2), ('b', 3), ('a', 4)]
