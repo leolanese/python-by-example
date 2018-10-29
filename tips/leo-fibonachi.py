9adfac39f95e4b99b655812561a0948c967aafd7
Leo Lanese <laneseleo@gmail.com>
2018-10-29 09:11:03 +0000

    leo fibonacci
    leo fibonacci

---
+++ tips/leo-fibonachi.py
@@ -0,0 +1,23 @@
+# Using looping technique
+def fibL(n):
+    a = 0
+    b = 1
+    for i in range(1,n+1):
+            c = a + b
+            print b
+            a = b
+            b = c
+# x=input('which fibonnaci do you want?')
+# fibL(x)  # 15 -> 610
+
+
+# Using recursion
+def fibR(n):
+   if n == 1:
+      return 1
+   elif n == 0:   
+      return 0            
+   else:                      
+      return fibR(n-1) + fibR(n-2)  
+x=input('which fibonnaci do you want?')
+print fibR(x) # 15 -> 610
