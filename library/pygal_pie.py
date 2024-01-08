71cf14aaedd24573470fda3315d2391d3e5de1a3
Leo Lanese <developer@leolanese.com>
2024-01-08 13:28:14 +0000

    Create pygal_pie.py

---
+++ library/pygal_pie.py
@@ -0,0 +1,10 @@
+import pygal
+
+pie_chart = pygal.Pie()
+pie_chart.title = 'Browser usage in February 2012 (in %)'
+pie_chart.add('IE', 19.5)
+pie_chart.add('Firefox', 36.6)
+pie_chart.add('Chrome', 36.3)
+pie_chart.add('Safari', 4.5)
+pie_chart.add('Opera', 2.3)
+pie_chart.render()
