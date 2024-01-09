de2c3fb7dc23ef967c8cb98d69c80d3d3b036da3
Leo Lanese <developer@leolanese.com>
2024-01-09 10:09:27 +0000

    Create tuple.py

---
+++ learning/tuple.py
@@ -0,0 +1,23 @@
+# tuples are created by placing a sequence of values separated by ‘comma’ with or without the use of parentheses for grouping the data sequence.
+
+Tuple1 = ()
+print("Initial empty Tuple: ")
+print(Tuple1)
+ 
+# Creating a Tuple
+# with the use of string
+Tuple1 = ('Leo', 'Lanese')
+print("\nTuple with the use of String: ")
+print(Tuple1)
+ 
+# Creating a Tuple with
+# the use of list
+list1 = [1, 2, 4, 5, 6]
+print("\nTuple using List: ")
+print(tuple(list1))
+ 
+# Creating a Tuple
+# with the use of built-in function
+Tuple1 = tuple('Leo')
+print("\nTuple with the use of function: ")
+print(Tuple1)
