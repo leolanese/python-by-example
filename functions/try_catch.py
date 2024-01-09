51431c5d2e37cf546378daf1c3bbcec7652544ed
Leo Lanese <developer@leolanese.com>
2024-01-09 09:43:56 +0000

    Update try_catch.py

--- functions/try_catch.py
+++ functions/try_catch.py
@@ -1,1 +1,19 @@
+# error handling: use this try/except pattern
 
+def test_for_int(num):
+
+    try:
+        num = int(num)
+        message = str(num) + " is an integer. Good job!"
+
+    # this exception handles any entry that was NOT an integer
+    except ValueError:
+        message = "Error: You did not provide a whole number."
+
+    return message
+
+# ask for user input
+response = input("Enter a number: ")
+
+# run the function, and print what is returned
+print( test_for_int(response) )
